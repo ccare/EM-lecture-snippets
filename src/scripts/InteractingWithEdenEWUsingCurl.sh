7b71b07cc628b97c87d50b521e12fb741cddc85a
Charles Care <c.p.care@gmail.com>
2010-11-27 18:04:31 +0000

    Adding eden-ws scripts

---
+++ src/scripts/InteractingWithEdenEWUsingCurl.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# create a new dependency world
+curl -X PUT http://localhost:8080/services/spaces/myspace -d ""
+# Define: a is b + c
+curl -X PUT http://localhost:8080/services/spaces/myspace/a -d "#b + #c"
+# Read observable a
+curl http://localhost:8080/services/spaces/myspace/a -w "\n"
+# define b and c
+curl -X PUT http://localhost:8080/services/spaces/myspace/b -d "1"
+curl -X PUT http://localhost:8080/services/spaces/myspace/c -d "2"
+# Read observable a
+curl http://localhost:8080/services/spaces/myspace/a -w "\n"
+# update b
+curl -X PUT http://localhost:8080/services/spaces/myspace/b -d "'a string '"
+# Read observable a
+curl http://localhost:8080/services/spaces/myspace/a -w "\n"
+# terminate my app
+curl -X DELETE http://localhost:8080/services/spaces/myspace -d ""
+# santity check that the state really has gone
+# read observable a
+curl http://localhost:8080/services/spaces/myspace/a -w "\n"
+
